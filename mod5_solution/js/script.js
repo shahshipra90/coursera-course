61b75d91ea873156d1667f8c1817e7b377497d2b
shahshipra90 <65304359+shahshipra90@users.noreply.github.com>
2020-07-16 16:44:12 +0530

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -19,7 +19,7 @@ var allCategoriesUrl =
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
